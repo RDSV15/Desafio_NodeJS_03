31feab91cb3628f5ae706130ee3a7a8a34de4d7f
RDSV15 <janseyrds@hotmail.com>
2022-03-10 14:58:13 -0300

    armazenamento de informacao e uso dela posteriormente

--- src/index.ts
+++ src/index.ts
@@ -38,24 +38,6 @@ app.listen(PORT, () => {
 
 	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
 	// R01, R02, R03, R04, R05
-
-	const nome1:string = 'João'
-	const nota1:number = 7
-	
-	const nome2:string = 'Pedro'
-	const nota2:number = 5
-	
-	const nome3:string = 'Gabriel'
-	const nota3:number = 7.5
-	
-	const nome4:string = 'José'
-	const nota4:number = 9
-	
-	const nome5:string = 'Tiago'
-	const nota5:number = 8.4
-	
-	const nome6:string = 'André'
-	const nota6:number = 8.6;
 	
 	const arr = [
 		{nome: 'João', nota: 7},
@@ -66,26 +48,47 @@ app.listen(PORT, () => {
 		{nome: 'André', nota: 8.6}
 	];
 	const count = arr.filter(Boolean).length;
+	//1,1
+
+	var readline = require('readline');
+	var resp = "";
+	//
+	var leitor = readline.createInterface({
+    	input: process.stdin,
+    	output: process.stdout
+	});
+	//2,1
 
 	const maxnota = arr.sort((val1,val2)=> {
 		return (val1.nota < val2.nota ) ? 1 : -1
-	})[0]
+	})[0];
+	//4,1
 	
 	console.log('Qual a quantidade de alunos?');
-	
+	//
 	console.log(count);
+	//1,2
+
+	leitor.question("Qual o seu nome?\n", function(answer: any) {
+    	var resp = answer;
+    	console.log("\nSua resposta '" + resp + "' foi armazenada");
+    	//
+		leitor.question("Olá " + resp + ", qual sua nota?\n", function(answer: any) {
+			var nota = answer;
+			console.log("\nSua resposta '" + nota + "' foi armazenada");
+			leitor.close();
+			//
+			console.log("Muito obrigado " + resp + ", pela sua resposta");
+			//3,2
+
+			console.log('Qual o aluno com a maior nota?');
+			//
+			console.log(maxnota);	
+			//4,2
 	
-	console.log('Qual seu nome e qual a sua nota?');
+		});
+	});
 
-	console.log(nome1  + ', nota:' + nota1)
-	console.log(nome2  + ', nota:' + nota2)
-	console.log(nome3  + ', nota:' + nota3)
-	console.log(nome4  + ', nota:' + nota4)
-	console.log(nome5  + ', nota:' + nota5)
-	console.log(nome6  + ', nota:' + nota6)
-
-	console.log('Qual o aluno com a maior nota?');
-
-	console.log(maxnota);	
 });
 
+	
